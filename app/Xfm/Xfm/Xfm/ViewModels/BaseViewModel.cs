a3fc61f5159a18de5e8076e9dcb3b70043a3a264
ThaisCristinaoa <52842898+ThaisCristinaoa@users.noreply.github.com>
2019-08-23 22:21:45 -0300

    versão 1

---
+++ app/Xfm/Xfm/Xfm/ViewModels/BaseViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xfm.ViewModels
+{
+    public abstract class BaseViewModel : BindableObject  
+    {
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get => isBusy; 
+            set { isBusy = value; OnPropertyChanged(); } // onPro avisa que o valr foi alterado
+
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
+        public async Task ExecuteBusyAction(Func<Task> theBuysAction)
+        {
+            if (isBusy)
+                return;
+            try
+            {
+                isBusy = true;
+                await theBuysAction();
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                System.Diagnostics.Debug.WriteLine(ex);
+
+#endif
+                throw;
+            }
+            finally
+            {
+                isBusy = false;
+            }
+        }
+
+        public virtual Task Initialize()
+            => Task.CompletedTask;
+    }
+}
---
+++ app/Xfm/Xfm/Xfm/ViewModels/HomeViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xfm.Models;
+using Xfm.Services;
+
+namespace Xfm.ViewModels
+{
+   public sealed class HomeViewModel : BaseViewModel
+    {
+        private readonly IItemService itemService;
+
+        private ObservableCollection<Item> items = new ObservableCollection<Item>();
+
+        public HomeViewModel()
+        {
+            itemService = new ItemService();
+        }
+
+        public ObservableCollection<Item> Items
+        {
+            get => items;
+            set { items = value; OnPropertyChanged(); }
+        }
+
+        public override async Task Initialize()
+        {
+            await ExecuteBusyAction(async ()=>
+            {
+                var itemCollection = await itemService.GetAll();
+
+                items.Clear();
+
+                foreach (var item in itemCollection)
+                    Items.Add(item);
+            });
+        }
+
+    }
+}
